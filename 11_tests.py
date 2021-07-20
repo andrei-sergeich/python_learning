b60d1da6a7a0dbd79b36c60e5aa77b10d90844b5
cmero <cmero@va-c5.kzn.21-school.ru>
2021-07-20 14:19:12 +0300

    add 11_tests

---
+++ 11_tests.py
@@ -0,0 +1,57 @@
+# Задание 1
+# Задан словарь. Напишите программу, 
+# которая будет выводить значение по заданному ключу.
+print("Задание 1. Вариант решения №1")
+
+# d = dict(Hello = 'Hi', Bye = 'Goodbye', List = 'Array')
+# print(d)
+
+dct = {'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5}
+key = input("Введите ключ: ")
+for i in dct.keys():
+    if i == key:
+        print(dct[i])
+
+print()
+print("Задание 1. Вариант решения №2")
+dct = {'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5}
+key = input("Введите ключ: ")
+print(dct.get(key))
+
+print()
+print("----------------------------")
+print()
+
+# Задание 2
+# Напишите программу, которая будет выполнять действие, обратное заданию 1. 
+# Программа должна производить поиск по значению и выдавать ключ.
+print("Задание 2. Вариант решения № 1")
+
+my_dict = {'Hello' : 'Hi', 'Bye' : 'Goodbye', 'List' : 'Array'}
+my_value = input("Введите значение: ")
+for key, value in my_dict.items():
+    if value == my_value:
+        print(key)
+
+print()
+print("Задание 2. Вариант решения № 2")
+
+my_dict = {'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5}
+my_value = int(input("Введите значение: "))
+for key in my_dict:
+    if my_dict[key] == my_value:
+        print(key)
+
+print()
+print("Задание 2. Вариант решения № 3")
+
+my_dict = {'Hello' : 'Hi', 'Bye' : 'Goodbye', 'List' : 'Array'}
+my_value = input("Введите значение: ")
+rev_dict = dict((v, k) for k, v in my_dict.items())
+for key in my_dict:
+    if my_dict[key] == my_value:
+        print(key)
+
+print()
+print("----------------------------")
+print()
